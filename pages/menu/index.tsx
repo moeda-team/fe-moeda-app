d87afa6ab6e64af71b0b00e1bad1c946e5d4652d
dhanakbar <maulana261100@gmail.com>
2025-06-22 16:06:23 +0700

    Adjust menu page if category null

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "@/components/sections";
 import { ProductCard } from "@/components/ui";
 import { mockProducts } from "@/mockProduct";
-import { getMenuByCategory } from "@/swr/get/products";
+import { getMenu, getMenuByCategory } from "@/swr/get/products";
 import { map } from "lodash";
 import { AnimatePresence } from "motion/react";
 import { useRouter } from "next/router";
@@ -17,6 +17,7 @@ const Menu = () => {
   const { category } = router.query;
   const { errorMenusByCategory, isLoadingMenusByCategory, menusByCategory } =
     getMenuByCategory(category as string);
+  const { errorMenu, isLoadingMenu, menu } = getMenu();
   const [openPopupOrder, setOpenPopupOrder] = useState<boolean>(false);
   const [productDetail, setProductDetail] = useState<any>({});
 
@@ -34,8 +35,23 @@ const Menu = () => {
       <div className="mt-8 px-4 text-lg font-semibold space-y-4">
         <h4>Menu</h4>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 pb-8">
-          {Array.isArray(menusByCategory) &&
+          {category &&
+            Array.isArray(menusByCategory) &&
             menusByCategory.map((product, index) => (
+              <ProductCard
+                key={index}
+                title={product.name}
+                description={product.desc}
+                image={product.img}
+                onAddToCart={() => {
+                  setOpenPopupOrder(true);
+                  setProductDetail(product);
+                }}
+              />
+            ))}
+          {!category &&
+            Array.isArray(menu) &&
+            menu.map((product, index) => (
               <ProductCard
                 key={index}
                 title={product.name}

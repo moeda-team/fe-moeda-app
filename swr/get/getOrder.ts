d438f71aca347fedf32b2ed23586dce87bb4afab
dhanakbar <maulana261100@gmail.com>
2025-06-25 20:20:09 +0700

    Change Endpoint get detail trx

--- swr/get/getOrder.ts
+++ swr/get/getOrder.ts
@@ -5,7 +5,7 @@ import { fetcher } from '../fetcher'
 export const getDetailOrder = (orderId: string | undefined) => {
   
   const shouldFetch = Boolean(orderId);
-  const url = shouldFetch ? `${API_URL}/transactions/order/status/${orderId}` : null;
+  const url = shouldFetch ? `${API_URL}/transactions/main/${orderId}` : null;
   
   const { data, error, isLoading }: { data: any, error: any, isLoading: boolean } = useSWR(url, fetcher)
 
